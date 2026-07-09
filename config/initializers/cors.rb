8202d0713b649a7d018ab553eefa925e112874a7
Marvin95122 <marvincruz868@gmail.com>
2026-07-09 13:57:00 -0600

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,13 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins "*"
+CORS_ACTIVO = ENV.fetch("CORS_ACTIVO", "true") == "true"
 
-    resource "*",
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+if CORS_ACTIVO
+  Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins "*"
+
+      resource "*",
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    end
   end
 end
